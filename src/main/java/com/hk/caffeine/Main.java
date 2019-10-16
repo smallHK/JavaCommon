13868da03664805ecb8b68c1b7352e07facbf505
zhangyy <zhangyy@yisa.com>
2019-10-16 16:59:03 +0800

    caffeine缓存测试代码

--- src/main/java/com/hk/caffeine/Main.java
+++ src/main/java/com/hk/caffeine/Main.java
@@ -3,17 +3,39 @@ package com.hk.caffeine;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 
-import java.net.CacheRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
 
+    //缓存设置
     public static void main(String[] args) {
-        Cache<Object, Object> cache = Caffeine.newBuilder()
-                .expireAfterWrite(10, TimeUnit.DAYS)
-                .maximumSize(10_000)
-                .build();
+        Cache<Integer, Integer> cache = Caffeine.newBuilder()
+                .expireAfterWrite(10, TimeUnit.SECONDS)
+                .maximumSize(5)
+                .build(k-> null);
+
+
+        List<Integer> keys = new ArrayList<>();
+        for(int i = 0; i < 10; i++) {
+            cache.put(i, i);
+            keys.add(i);
+        }
+
+        try {
+            Thread.sleep(5 * 100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Map<Integer, Integer> values = cache.getAllPresent(keys);
+        for(Integer key: keys) {
+            System.out.println("key: " + key);
+            System.out.println("value: " + values.get(key));
+        }
 
     }
 }
