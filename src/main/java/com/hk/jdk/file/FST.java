0393bb27032513f8581be1a60be416670ede8602
smallHK <2373531616@qq.com>
2020-02-29 19:38:40 +0800

    查看jrt文件系统

--- src/main/java/com/hk/jdk/file/FST.java
+++ src/main/java/com/hk/jdk/file/FST.java
@@ -1,12 +1,37 @@
 package com.hk.jdk.file;
 
 import java.io.IOException;
-import java.nio.file.FileStore;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.*;
+import java.util.HashMap;
 
 public class FST {
+
+
+    /**
+     * 查看jrt文件系统
+     */
+    private void jrtFileSystem() {
+
+        try {
+            FileSystem fs = FileSystems.newFileSystem(URI.create("jrt:/"), new HashMap<>());
+            for(Path root: fs.getRootDirectories()) {
+                System.out.println(root);
+            }
+            System.out.println(fs.getSeparator());
+
+
+            URL classUrl = new URL("jrt:/java.base/java/lang/Object.class");
+            InputStream input = classUrl.openStream();
+            byte[] bytes = input.readAllBytes();
+            System.out.println("Object.class file size: " + bytes.length);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     //默认文件系统属性查看
@@ -34,6 +59,7 @@ public class FST {
     public static void main(String[] args) {
 
         var main = new FST();
-        main.defaultFileSystemTest();
+//        main.defaultFileSystemTest();
+        main.jrtFileSystem();
     }
 }
