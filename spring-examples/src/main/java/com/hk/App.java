464b1ce33bde99c9e2fbc0b60f1474d3687c2731
smallHK <2373531616@qq.com>
2020-03-06 19:08:49 +0800

    Spring FileSystemResource测试
    2020/03/06

--- spring-examples/src/main/java/com/hk/App.java
+++ spring-examples/src/main/java/com/hk/App.java
@@ -2,6 +2,10 @@ package com.hk;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import java.io.File;
 
 /**
  * Hello world!
@@ -9,10 +13,27 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+
+    //Spring Resource资源测试
+    private static void firstResource() {
+        Resource resource = new FileSystemResource(new File("D:\\Java\\openjdk\\README"));
+        System.out.println(resource.getDescription());
+        System.out.println(resource.getFilename());
+        System.out.println(resource.exists());
+        System.out.println(resource.isFile());
+    }
+
+
+
+    private static void firstContainer() {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
         Service service = ctx.getBean(Service.class);
         service.run();
+    }
+
+
+    public static void main( String[] args )
+    {
+        firstResource();
     }
 }
