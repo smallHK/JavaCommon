32262d15221f639883a3d2d5addb9ca71e9677c5
smallHK <2373531616@qq.com>
2020-03-15 18:52:52 +0800

    netty nio server通道注册事件循环，执行端口绑定

--- src/main/java/com/hk/netty/ChannelFactoryT.java
+++ src/main/java/com/hk/netty/ChannelFactoryT.java
@@ -1,10 +1,47 @@
 package com.hk.netty;
 
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ReflectiveChannelFactory;
+import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
+import java.net.InetSocketAddress;
+
 public class ChannelFactoryT {
+
+
+    /**
+     * nio server通道注册事件循环，执行端口绑定
+     */
+    private static void firstRegister() {
+        ReflectiveChannelFactory<NioServerSocketChannel> factory = new ReflectiveChannelFactory<>(NioServerSocketChannel.class);
+        Channel channel = factory.newChannel();
+
+        System.out.println(channel.isOpen());
+        System.out.println(channel.isActive());
+        System.out.println(channel.isRegistered());
+        System.out.println();
+
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        ChannelFuture cf = group.register(channel);
+        cf.addListener(f -> {
+            System.out.println(channel.isOpen());
+            System.out.println(channel.isActive());
+            System.out.println(channel.isRegistered());
+            System.out.println();
+
+            ChannelFuture bcf = channel.bind(new InetSocketAddress("127.0.0.1", 8080));
+            bcf.addListener(fb -> { //回调地狱？
+                System.out.println(channel.isOpen());
+                System.out.println(channel.isActive());
+                System.out.println(channel.isRegistered());
+                System.out.println("完成绑定！");
+            });
+            System.out.println("绑定端口！");
+
+        });
+    }
 
     private static void firstChannel() {
         ReflectiveChannelFactory<NioServerSocketChannel> factory = new ReflectiveChannelFactory<>(NioServerSocketChannel.class);
@@ -12,9 +49,13 @@ public class ChannelFactoryT {
         System.out.println(channel.isOpen());
         System.out.println(channel.isActive());
         System.out.println(channel.isRegistered());
+        System.out.println();
+
+
     }
 
     public static void main(String[] args) {
-        firstChannel();
+//        firstChannel();
+        firstRegister();
     }
 }
