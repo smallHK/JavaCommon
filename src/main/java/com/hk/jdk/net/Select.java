2c88d26d59b4bbfb5dd84e694d89469f26b90706
smallHK <2373531616@qq.com>
2020-03-17 21:15:39 +0800

    jdk，创建ServerSocketChannel，绑定端口

--- src/main/java/com/hk/jdk/net/Select.java
+++ src/main/java/com/hk/jdk/net/Select.java
@@ -1,11 +1,23 @@
 package com.hk.jdk.net;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
 
 public class Select {
 
 
+
+    private static void firstServerChannel() {
+        try {
+            ServerSocketChannel ch = ServerSocketChannel.open();
+            ch.bind(new InetSocketAddress(8080));
+            System.out.println("完成绑定");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private static void firstSelector() {
         try(Selector selector = Selector.open();) {
@@ -17,5 +29,6 @@ public class Select {
 
     public static void main(String[] args) {
 
+        firstServerChannel();
     }
 }
