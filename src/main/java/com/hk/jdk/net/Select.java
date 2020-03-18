53f43d0f7c4e4e3acf13bac0c610ad7fab0723ef
smallHK <2373531616@qq.com>
2020-03-18 23:24:08 +0800

    jdk，创建SocketChannel，连接ServerSocketChannel，发送数据通信

--- src/main/java/com/hk/jdk/net/Select.java
+++ src/main/java/com/hk/jdk/net/Select.java
@@ -2,18 +2,46 @@ package com.hk.jdk.net;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 
 public class Select {
 
+    //客户端
+    private static void firstChannel() {
+        try {
+            SocketChannel channel = SocketChannel.open();
+            if(channel.connect(new InetSocketAddress(8080))) {
+                System.out.println("客户端成功连接！");
+                channel.write(ByteBuffer.wrap(new String("Hello world!").getBytes()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+    }
 
+    //服务器
     private static void firstServerChannel() {
         try {
             ServerSocketChannel ch = ServerSocketChannel.open();
             ch.bind(new InetSocketAddress(8080));
             System.out.println("完成绑定");
+
+            new Thread(() -> {
+                SocketChannel socketChannel = null;
+                try {
+                    socketChannel = ch.accept(); //阻塞操作！
+                    ByteBuffer bf = ByteBuffer.allocate(1000);
+                    socketChannel.read(bf);
+                    System.out.println( bf.position());
+                    socketChannel.finishConnect();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -30,5 +58,6 @@ public class Select {
     public static void main(String[] args) {
 
         firstServerChannel();
+        firstChannel();
     }
 }
