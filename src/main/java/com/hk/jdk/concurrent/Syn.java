3bf80c6a424dc746a2db7c9d7716edd78578f5d8
smallHK <2373531616@qq.com>
2020-04-06 22:36:15 +0800

    jdk，AbstractQueuedSynchronizer查看设置内部同步状态

--- src/main/java/com/hk/jdk/concurrent/Syn.java
+++ src/main/java/com/hk/jdk/concurrent/Syn.java
@@ -3,12 +3,29 @@ package com.hk.jdk.concurrent;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
 public class Syn {
+
+    private static void syncState() {
+        class S extends AbstractQueuedSynchronizer {
+            void run() {
+                System.out.println("状态： " + getState());
+                setState(121323);
+                System.out.println("状态： " + getState());
+                System.out.println(compareAndSetState(1, 123));
+                System.out.println(compareAndSetState(121323, 123));
+                System.out.println("状态： " + getState());
+            }
+
+        }
+        S s = new S();
+        s.run();
+    }
+
     private static void firstSync() {
         class S extends AbstractQueuedSynchronizer {
             @Override
             protected boolean tryAcquire(int arg) {
                 System.out.println("start: " + getState());
-                if(compareAndSetState(0, 1)) {
+                if (compareAndSetState(0, 1)) {
                     setExclusiveOwnerThread(Thread.currentThread());
                     System.out.println("success: " + getState());
                     return true;
@@ -19,8 +36,8 @@ public class Syn {
 
             @Override
             protected boolean tryRelease(int arg) {
-                if(!isHeldExclusively())
-                    throw  new IllegalMonitorStateException();
+                if (!isHeldExclusively())
+                    throw new IllegalMonitorStateException();
                 setExclusiveOwnerThread(null);
                 setState(0);
                 return true;
@@ -38,6 +55,7 @@ public class Syn {
 
     public static void main(String[] args) {
 
-        firstSync();
+//        firstSync();
+        syncState();
     }
 }
