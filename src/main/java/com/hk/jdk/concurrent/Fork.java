9483e4802c136bf893e351a1e86c931ff49891b5
smallHK <2373531616@qq.com>
2020-04-12 18:57:13 +0800

    jdk，公共ForkJoinPool获取ForkJoin线程工厂，使用线程工厂创建线程

--- src/main/java/com/hk/jdk/concurrent/Fork.java
+++ src/main/java/com/hk/jdk/concurrent/Fork.java
@@ -1,6 +1,8 @@
 package com.hk.jdk.concurrent;
 
+import java.lang.reflect.Field;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinWorkerThread;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -8,6 +10,24 @@ import java.util.concurrent.TimeUnit;
  * 2020/4/11 22:44
  */
 public class Fork {
+
+    private static void commonFactory() {
+        ForkJoinPool pool = ForkJoinPool.commonPool();
+        ForkJoinPool.ForkJoinWorkerThreadFactory factory = pool.getFactory();
+        ForkJoinWorkerThread thread = factory.newThread(pool);
+        System.out.println(thread.hashCode());
+        try {
+            Class clazz = thread.getClass().getSuperclass();
+            System.out.println(clazz.getName());
+            Field field = clazz.getDeclaredField("target");
+            field.setAccessible(true);
+            Runnable task = (Runnable)field.get(thread);
+            System.out.println(task);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     private static void commonPool() {
         ForkJoinPool pool = ForkJoinPool.commonPool();
@@ -42,6 +62,7 @@ public class Fork {
 
     public static void main(String[] args) {
 
-        commonPool();
+//        commonPool();
+        commonFactory();
     }
 }
